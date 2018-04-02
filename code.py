784ee9df28d8aea34c090697818b803fbf9a2f75
Jonathan Ploudre <jploudre@gmail.com>
2018-04-01 21:25:03 -0700

    Just 5 sounds,
    Also add while a.playing

--- code.py
+++ code.py
@@ -1,5 +1,3 @@
-# CircuitPlaygroundExpress_NeoPixel
-
 import board
 import neopixel
 import time
@@ -18,22 +16,17 @@ spkrenable.direction = Direction.OUTPUT
 spkrenable.value = True
 
 # make the input cap sense pads
-capPins = (board.A1, board.A2, board.A3, board.A4, board.A5,
-           board.A6, board.A7)
+capPins = (board.A1, board.A2, board.A3, board.A4, board.A5)
 
 touchPad = []
-for i in range(7):
+for i in range(5):
     touchPad.append(touchio.TouchIn(capPins[i]))
 
 # The seven files assigned to the touchpads
 audiofiles = ["T00RAND0.wav", "T00RAND1.wav", "T00RAND2.WAV",
-               "T00RAND3.WAV", "T10HOLDL.wav", "",
-               ""]
-
+               "T00RAND3.WAV", "T10HOLDL.wav"]
 
 def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
     if pos < 85:
         return (int(pos*3), int(255 - (pos*3)), 0)
     elif pos < 170:
@@ -46,19 +39,24 @@ def wheel(pos):
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(len(pixels)):
-            idx = int ((i * 256 / len(pixels)) + j*10)
+            idx = int ((i * 256 / len(pixels)) + j*2)
             pixels[i] = wheel(idx & 255)
         pixels.show()
         time.sleep(wait)
+    pixels.fill((0,0,0))
 
 def play_file(filename):
     print("playing file "+filename)
     f = open(filename, "rb")
     a = audioio.AudioOut(board.A0, f)
     a.play()
+    while a.playing:
+        pass
+    print("finished")
 
 while True:
-    for i in range(7):
+    for i in range(5):
         if touchPad[i].value:
             play_file(audiofiles[i])
+            rainbow_cycle(0.01)
 
